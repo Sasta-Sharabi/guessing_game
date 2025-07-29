896de0ae25d85e480ec024cbc151f61ffff17f81
Sasta-Sharabi <jokerthief002@gmail.com>
2025-07-29 11:59:11 +0000

    Final Guessing Game

--- src/guess_backend/src/lib.rs
+++ src/guess_backend/src/lib.rs
@@ -1,4 +1,26 @@
-#[ic_cdk::query]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
+use ic_cdk::api::management_canister::main::raw_rand;
+
+#[ic_cdk::update]
+pub async fn play(guess: String) -> (bool, u8) {
+    let random_bytes = match raw_rand().await {
+        Ok((bytes,)) => bytes,
+        Err(_) => return (false, 0),
+    };
+
+    let secret_number = (random_bytes[0] % 20) + 1;
+
+    let guess: u8 = match guess.trim().parse() {
+        Ok(num) => num,
+        Err(_) => 0,
+    };
+
+    if guess != 0 {
+        (check_guess(guess, secret_number), secret_number)
+    } else {
+        (false, secret_number)
+    }
+}
+
+fn check_guess(guess: u8, secret_number: u8) -> bool {
+    guess == secret_number
 }
--- src/guess_frontend/src/App.jsx
+++ src/guess_frontend/src/App.jsx
@@ -1,30 +1,56 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { guess_backend } from 'declarations/guess_backend';
+import Confetti from 'react-confetti';
+import { useWindowSize } from '@react-hook/window-size';
+import "./style/App.css";
 
 function App() {
-  const [greeting, setGreeting] = useState('');
+  const [win, setWin] = useState(false);
+  const [played, setPlayed] = useState(false);
+  const [secret, setSecret] = useState(0);
+  const [width, height] = useWindowSize(); 
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    const name = event.target.elements.name.value;
-    guess_backend.greet(name).then((greeting) => {
-      setGreeting(greeting);
-    });
-    return false;
+  async function handleSubmit(formData) {
+    const number = formData.get("number");
+    const returned = await guess_backend.play(number);
+
+    setWin(returned[0]);
+    setSecret(returned[1]);
+    setPlayed(true);
   }
 
   return (
-    <main>
-      <img src="/logo2.svg" alt="DFINITY logo" />
-      <br />
-      <br />
-      <form action="#" onSubmit={handleSubmit}>
-        <label htmlFor="name">Enter your name: &nbsp;</label>
-        <input id="name" alt="Name" type="text" />
+    <>
+      <h1>Welcome To Guessing Game</h1>
+      <div>The guessing range is from 1 to 20</div>
+
+      <form
+        onSubmit={async (e) => {
+          e.preventDefault();
+          const formData = new FormData(e.target);
+          await handleSubmit(formData);
+          e.target.reset();
+        }}
+      >
+        <label htmlFor="number">Enter your number: &nbsp;</label>
+        <input id="number" name="number" type="number" placeholder="0" />
         <button type="submit">Click Me!</button>
       </form>
-      <section id="greeting">{greeting}</section>
-    </main>
+
+      {played && !win && (
+        <>
+          <div>The secret number was: {secret}</div>
+          <div className="played-lose">You Lost</div>
+        </>
+      )}
+
+      {played && win && (
+        <>
+          <div className="played-win">You Guessed Right!!</div>
+          <Confetti width={width} height={height} />
+        </>
+      )}
+    </>
   );
 }
 
